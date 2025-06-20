0a8fb051e074f843273eea2f5be978c75902ca60
Krablante <gkrablante@gmail.com>
2025-06-20 16:33:35 +0100

    PWA upd test

--- index.tsx
+++ index.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // Assuming this file exists and is needed
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
